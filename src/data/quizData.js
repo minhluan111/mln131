9a865ca99195ab3af928ab55dd088aa00885f8e9
minhluan2510 <luanqmse182755@fpt.edu.vn>
2026-07-15 07:15:39 +0700

    Update quizData.js

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -3,70 +3,70 @@ export const quizData = [
     "id": 1,
     "difficulty": "Dễ",
     "question": "Lực lượng nào trực tiếp tham gia sản xuất công nghiệp và vận hành máy móc?",
-    "scrambled": "iaiG - pấC - gnôC - nâhN",
+    "scrambled": "igai - cpấ - ôcgn - hnân",
     "correct": "Giai cấp công nhân"
   },
   {
     "id": 2,
     "difficulty": "Trung bình",
     "question": "Giai cấp nông dân giữ vai trò quan trọng trong việc bảo đảm điều gì cho quốc gia?",
-    "scrambled": "nA - hniN - gnưLơ - cựhT",
+    "scrambled": "an - nihn - gưlnơ - hựct",
     "correct": "An ninh lương thực"
   },
   {
     "id": 3,
     "difficulty": "Dễ",
     "question": "Đội ngũ trí thức đóng góp chủ yếu vào lĩnh vực nào để đổi mới sáng tạo?",
-    "scrambled": "aoKh - cọH - gnôC - ệhgN",
+    "scrambled": "okha - ọch - gônc - hệgn",
     "correct": "Khoa học công nghệ"
   },
   {
     "id": 4,
     "difficulty": "Trung bình",
     "question": "Sự liên kết chặt chẽ giữa ba lực lượng giúp phát triển đồng bộ lĩnh vực nào?",
-    "scrambled": "táhP - nểirT - hniK - ếT",
+    "scrambled": "hptá - nrtểi - ihkn - ết",
     "correct": "Phát triển kinh tế"
   },
   {
     "id": 5,
     "difficulty": "Khó",
     "question": "Liên minh công - nông - trí góp phần giữ vững điều gì cho đất nước?",
-    "scrambled": "nỔ - hnịĐ - hníhC - ịrT",
+    "scrambled": "nổ - hđnị - nhích - rịt",
     "correct": "Ổn định chính trị"
   },
   {
     "id": 6,
     "difficulty": "Trung bình",
     "question": "Trong bối cảnh hiện nay, đội ngũ trí thức giúp đẩy nhanh quá trình gì?",
-    "scrambled": "nểyuCh - iổĐ - ốS",
+    "scrambled": "nyuểch - ổđi - ốs",
     "correct": "Chuyển đổi số"
   },
   {
     "id": 7,
     "difficulty": "Dễ",
     "question": "Trong thời kỳ hiện đại hóa, nông dân cần tham gia xây dựng mô hình gì ở quê hương?",
-    "scrambled": "gnôN - nôhT - iớM",
+    "scrambled": "gônn - nhôt - ớim",
     "correct": "Nông thôn mới"
   },
   {
     "id": 8,
     "difficulty": "Khó",
     "question": "Liên minh công - nông - trí là nền tảng quan trọng để phát triển nền kinh tế gì?",
-    "scrambled": "hniK - ếT - irT - cứhT",
+    "scrambled": "hink - ết - itr - chtứ",
     "correct": "Kinh tế tri thức"
   },
   {
     "id": 9,
     "difficulty": "Khó",
     "question": "Muốn đất nước phát triển bền vững thì phải củng cố sức mạnh nào của toàn dân?",
-    "scrambled": "iạĐ - nàĐo - tếK",
+    "scrambled": "điạ - ànđo - tkế",
     "correct": "Đại đoàn kết"
   },
   {
     "id": 10,
     "difficulty": "Dễ",
     "question": "Việt Nam đang đẩy mạnh công cuộc gì để nâng cao đời sống nhân dân?",
-    "scrambled": "gnôC - pệihgN - aóH",
+    "scrambled": "gônc - hpệign - ahó",
     "correct": "Công nghiệp hóa"
   }
 ];
