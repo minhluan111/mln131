72f359ff278705d49720d76a3396b115f6a70a1f
ThuaanNguyeen111 <nhoxmymap74@gmail.com>
2026-07-16 07:58:05 +0700

    update data

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -4,7 +4,7 @@ export const quizData = [
     difficulty: "Dễ",
     question:
       "Mức lạm phát phi mã của Việt Nam vào năm 1986 đạt con số kỷ lục là bao nhiêu?",
-    scrambled: "năhpt - mătr - ảby - ưmtơi - myơi - ăub - mtră",
+    scrambled: "ưmơi - ầnph - mắrt - ưt - ảyB - ảyb - mărt",
     correct: "Bảy trăm bảy mươi tư phần trăm",
   },
   {
@@ -12,21 +12,21 @@ export const quizData = [
     difficulty: "Dễ",
     question:
       "Đại hội đại biểu toàn quốc lần thứ mấy của Đảng đã khởi đầu công cuộc Đổi mới?",
-    scrambled: "iộh - ạđi - IV",
+    scrambled: "IV - ộhi - ạiĐ",
     correct: "Đại hội VI",
   },
   {
     id: 3,
     difficulty: "Trung bình",
     question: "Trước năm 1986, nền kinh tế Việt Nam vận hành theo cơ chế nào?",
-    scrambled: "auqo - êtpn - cắp - êuil - unrt - oba",
+    scrambled: "uêil - oab - ậpT - unrt - nauq - ấpc",
     correct: "Tập trung quan liêu bao cấp",
   },
   {
     id: 4,
     difficulty: "Khó",
     question: "Ai là người được bầu làm Tổng Bí thư tại Đại hội VI năm 1986?",
-    scrambled: "nilh - ăvn - ếynugn",
+    scrambled: "hnLi - uyễnNg - ănV",
     correct: "Nguyễn Văn Linh",
   },
   {
@@ -34,7 +34,7 @@ export const quizData = [
     difficulty: "Trung bình",
     question:
       "Đổi mới kinh tế là chuyển sang nền kinh tế hàng hóa nhiều thành phần, vận động theo cơ chế gì?",
-    scrambled: "ờtrgnưi - ơc - hếđ - ịht",
+    scrambled: "ịht - ờưnrtg - ơc - ếch",
     correct: "Cơ chế thị trường",
   },
   {
@@ -42,7 +42,7 @@ export const quizData = [
     difficulty: "Dễ",
     question:
       "Năm nào Việt Nam lần đầu tiên xuất khẩu gạo, chính thức chấm dứt thời kỳ thiếu đói?",
-    scrambled: "1899 - măsn",
+    scrambled: "1989 - măN",
     correct: "Năm 1989",
   },
   {
@@ -50,7 +50,7 @@ export const quizData = [
     difficulty: "Khó",
     question:
       "Trong lần đầu tiên xuất khẩu gạo năm 1989, Việt Nam đã xuất khẩu được bao nhiêu tấn?",
-    scrambled: "tấn - 13,7 - iệutr",
+    scrambled: "ấnt - 1,37 - ệiutr",
     correct: "1,37 triệu tấn",
   },
   {
@@ -58,7 +58,7 @@ export const quizData = [
     difficulty: "Trung bình",
     question:
       "Tinh thần đối ngoại mới của Việt Nam từ sau Đại hội VI được xác định là gì?",
-    scrambled: "ảnđ - cạc - tấc - bạmn - cốun - ớưcn - àml - iớv - ướt",
+    scrambled: "ớiv - ớưcn - àml - ácc - uốnM - ấtt - ạnb - ảc",
     correct: "Muốn làm bạn với tất cả các nước",
   },
   {
@@ -66,7 +66,7 @@ export const quizData = [
     difficulty: "Khó",
     question:
       "Sản lượng lúa của Việt Nam đã tăng lên bao nhiêu triệu tấn vào năm 1990?",
-    scrambled: "tấn - 2,15 - iệutr",
+    scrambled: "ệiutr - ấnt - 21,5",
     correct: "21,5 triệu tấn",
   },
   {
@@ -74,7 +74,7 @@ export const quizData = [
     difficulty: "Trung bình",
     question:
       "Việt Nam chính thức gia nhập tổ chức thương mại nào vào năm 2007?",
-    scrambled: "ớiig - hứcđ - ạmi - cớt - ươthn - gnề",
+    scrambled: "ớiig - ươnth - ổT - ứcch - ếth - ạmgi",
     correct: "Tổ chức thương mại thế giới",
   },
 ];
